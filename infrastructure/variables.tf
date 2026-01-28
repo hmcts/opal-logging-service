95d8ac9bb8366d45f87e60d32fae621875eda377
52960 <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
2026-01-28 17:19:00 +0000

    Updating Terraform Formatting

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -22,7 +22,7 @@ variable "jenkins_AAD_objectId" {
 
 variable "pgsql_databases" {
   description = "List of PostgreSQL databases to create"
-  type        = list(object({
+  type = list(object({
     name = string
   }))
   default = [
@@ -35,7 +35,7 @@ variable "pgsql_databases" {
 
 variable "db_name" {
   description = "Name of the app database"
-  default = "opal-logging-db"
+  default     = "opal-logging-db"
 }
 
 variable "pgsql_server_configuration" {
