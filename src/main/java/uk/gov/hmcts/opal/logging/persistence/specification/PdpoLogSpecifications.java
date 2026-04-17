4e3697d054573074e7f87cd3c528736e41044b0e
Sarah Wall <259313819+sarahtw-cgi@users.noreply.github.com>
2026-04-17 12:14:50 +0100

    PO-3560 - add individual id filter

--- src/main/java/uk/gov/hmcts/opal/logging/persistence/specification/PdpoLogSpecifications.java
+++ src/main/java/uk/gov/hmcts/opal/logging/persistence/specification/PdpoLogSpecifications.java
@@ -3,11 +3,15 @@ package uk.gov.hmcts.opal.logging.persistence.specification;
 import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.JoinType;
 import java.util.Optional;
+
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Subquery;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.opal.logging.domain.PdpoCategory;
 import uk.gov.hmcts.opal.logging.persistence.entity.PdpoIdentifierEntity;
 import uk.gov.hmcts.opal.logging.persistence.entity.PdpoLogEntity;
+import uk.gov.hmcts.opal.logging.persistence.entity.PdpoLogIndividualEntity;
 
 /**
  * Factory for {@link Specification} instances used to query {@link PdpoLogEntity}.
@@ -19,6 +23,7 @@ public class PdpoLogSpecifications extends EntitySpecs<PdpoLogEntity> {
         return Specification.allOf(specificationList(
             createdByFilter(criteria),
             notBlank(criteria.businessIdentifier()).map(PdpoLogSpecifications::businessIdentifier),
+            notBlank(criteria.individualIdentifier()).map(PdpoLogSpecifications::individualIdentifier),
             notNull(criteria.category()).map(PdpoLogSpecifications::category)
         ));
     }
@@ -40,6 +45,22 @@ public class PdpoLogSpecifications extends EntitySpecs<PdpoLogEntity> {
         return (root, query, cb) -> {
             Join<PdpoLogEntity, PdpoIdentifierEntity> join = root.join("businessIdentifier", JoinType.INNER);
             return cb.equal(join.get("businessIdentifier"), businessIdentifier);
+        };
+    }
+
+    private static Specification<PdpoLogEntity> individualIdentifier(String individualId) {
+        return (root, query, cb) -> {
+            query.distinct(true);
+
+            Subquery<Long> subquery = query.subquery(Long.class);
+            Root<PdpoLogEntity> correlatedRoot = subquery.correlate(root);
+            Join<PdpoLogEntity, PdpoLogIndividualEntity> individuals =
+                correlatedRoot.join("individuals", JoinType.INNER);
+
+            subquery.select(correlatedRoot.get("id"))
+                .where(cb.equal(individuals.get("individualIdentifier"), individualId));
+
+            return cb.exists(subquery);
         };
     }
 
